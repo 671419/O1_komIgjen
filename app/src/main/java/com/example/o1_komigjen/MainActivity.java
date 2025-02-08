6d3cdfb28e38bd0ef9b0745e525663aa4b4ca978
671419 <671419@stud.hvl.no>
2025-02-08 11:02:39 +0100

    Oppsett av grunnbrikker

--- app/src/main/java/com/example/o1_komigjen/MainActivity.java
+++ app/src/main/java/com/example/o1_komigjen/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.o1_komigjen;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +11,24 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnGallery, btnQuiz;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        btnGallery = findViewById(R.id.btnGallery);
+        btnQuiz = findViewById(R.id.btnQuiz);
+
+        btnGallery.setOnClickListener(view -> {
+            startActivity(new Intent(MainActivity.this, GalleryActivity.class));
         });
+
+        btnQuiz.setOnClickListener(view -> {
+            startActivity(new Intent(MainActivity.this, QuizActivity.class));
+        });
+
     }
 }
